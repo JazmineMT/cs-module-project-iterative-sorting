2ebef43a5d437af57fe8a12c131922119ae36857
JazmineMT <jazmineamcginnis@gmail.com>
2020-09-14 13:06:16 -0700

    Stretch -kinda

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,61 @@
+
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(len(arr)):
+        # cur_index = i
+        # smallest_index = cur_index
+        min_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(i + 1 , len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+            
+        temp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = temp
 
 
         # TO-DO: swap
         # Your code here
 
-    return arr
+        
+    return arr 
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    for passnum  in range(len(arr) -1 , 0 , -1):
+        for i in range(passnum):
+            if arr[i] > arr[ i + 1]:
+                temp = arr[i]
+                arr[i] = arr[ i + 1]
+                arr[i + 1] = temp
+    
 
     return arr
+
+# faster bubble search -> shortbubbleSort
+#it tests to see if it's been sorted therefore the final search through it not neccessary 
+def short_bubble_sort(arr):
+    exchanges = True
+    passnum = len(arr) - 1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if arr[i] > arr[ i + 1]:
+                exchanges = True
+                temp = arr[i]
+                arr[i] =  arr[i + 1]
+                arr[ i + 1] = temp
+        passnum = passnum - 1
+    return arr 
+
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +74,19 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
-
-
-    return arr
+def counting_sort(array, maxval= None):
+    """in-place counting sort"""
+    if maxval == None:
+        return array
+    
+    n = len(array)
+    m = maxval + 1
+    count = [0] * m               # init with zeros
+    for a in array:
+        count[a] += 1             # count occurences
+    i = 0
+    for a in range(m):            # emit
+        for c in range(count[a]): # - emit 'count[a]' copies of 'a'
+            array[i] = a
+            i += 1
+    return array
